880c5ed41ae12cf3d68a65d95bc5285029c5367c
molexandev01~ <molexandev@gmail.com>
2023-07-14 21:23:42 +0300

    added filter section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,75 @@
-$(function(){
+$(function () {
    
-})
+    var $range = $(".calculate__input-filter"),
+        $input = $(".calculate__input-num"),
+        $input2 = $(".calculate__input-num-2"),
+        instance,
+        min = 50000,
+        max = 1000000;
+
+
+    $range.ionRangeSlider({
+        skin: "big",
+        type: "single",        
+        min: min,
+        max: max,
+        step: 10000,
+        from: 400000,
+        onStart: function (data) {
+            $input.prop("value", data.from);
+            $input2.prop("value", formatValue(data.from * 1.28, 2));
+        },
+        onChange: function (data) {
+            $input.prop("value", data.from);
+            $input2.prop("value", formatValue(data.from * 1.28, 2));
+        },
+    });
+
+    instance = $range.data("ionRangeSlider");
+
+    $input.on("change keyup", function () {
+        var val = $(this).prop("value");
+
+        // validate
+        if (val < min) {
+            val = min;
+        } else if (val > max) {
+            val = max;
+        }
+
+        instance.update({
+            from: val,
+        });
+    });
+
+    function formatValue(value, decimalPlaces) {
+        return Number(value.toFixed(decimalPlaces));
+    }
+
+    $(".clients-stories__slider").slick({
+      dots: true,
+      arrows: true,
+      speed: 3000,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 100,
+      draggable: true,
+  });
+
+
+    $(".rating-1").rateYo({
+      starWidth: "22px",
+      starHeight: "22px",
+      ratedFill: "#C8B178",
+      rating: 5,
+      spacing: "5px"
+    });
+
+    $(".rating-2").rateYo({
+      starWidth: "17px",
+      ratedFill: "#C8B178",
+      rating: 5,
+      spacing: "3px"
+    });
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -59,6 +59,9 @@ function styles() {
 function scripts() {
    return src([
       'node_modules/jquery/dist/jquery.js',
+      'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/rateyo/src/jquery.rateyo.js',
       'app/js/main.js'
    ])
    .pipe(concat('main.min.js'))
